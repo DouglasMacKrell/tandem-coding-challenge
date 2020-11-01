81a4025d57bb4b47e181934036c994fbc9fa05f1
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-11-01 17:27:57 -0500

    Json parsed and compiled

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './App.css';
-import Trivia from './Apprentice_TandemFor400_Data.json'
+import "./App.css";
+import Trivia from "./Apprentice_TandemFor400_Data.json";
 
 const App = () => {
-  const [trivia, setTrivia] = useState(Trivia)
-  
-  const fetchTrivia = () => {
-    let result = []
+  const [trivia, setTrivia] = useState(Trivia);
+  const [gameQuestions, setGameQuestions] = useState([])
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const compileTrivia = () => {
+    let result = [];
     for (let question of trivia) {
-      console.log("gotcha")
+      let questions = question.incorrect;
+      let correct = question.correct;
+      questions.push(correct);
+      let myObj = {
+        question: question.question,
+        choices: questions,
+        correct: question.correct,
+      };
+      result.push(myObj);
+    }
+    console.log(result);
+    setTrivia(result);
+  };
+
+  const selectQuestions = () => {
+    let result = []
+
+    for (let i = 0; i < 10; i++) {
+      
     }
   }
-  console.log(trivia)
+
+  useEffect(() => {
+    compileTrivia();
+  }, []);
+
+  console.log(trivia);
   return (
     <div className="App">
       Hello World
-      <button onClick={fetchTrivia}>GO</button>
+      <br />
+      <button>GO</button>
     </div>
   );
-}
+};
 
 export default App;
