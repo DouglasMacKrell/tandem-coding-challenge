0e13356f388e56255828728c95953da905744bf1
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-11-01 21:12:20 -0500

    right and wromng guess color change handled

--- client/src/components/QuestionCard.jsx
+++ client/src/components/QuestionCard.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './QuestionCard.css'
 
 const QuestionCard = ({ questionNr, totalQuestions, question, choices, userAnswer, callback }) => {
+    // const [status, setStatus] = useState(null)
+
+    // const changeStatus = (choice) => {
+    //     if (userAnswer) {
+    //         if (userAnswer.correctAnswer === choice) {
+    //             setStatus("correct-answer")
+    //         } else if (userAnswer.correctAnswer !== choice && userAnswer.answer === choice) {
+    //             setStatus
+    //         }
+    //     }
+    // }
+
     return (
       <div className="question-card">
         <p className="number">
@@ -20,7 +32,16 @@ const QuestionCard = ({ questionNr, totalQuestions, question, choices, userAnswe
                 disabled={userAnswer ? true : false}
                 value={choice}
                 onClick={callback}
-                className="multiple-choice-btn"
+                className={
+                  userAnswer
+                    ? userAnswer.correctAnswer === choice
+                      ? "multiple-choice-btn correct"
+                      : userAnswer.correctAnswer !== choice &&
+                        userAnswer?.answer === choice
+                      ? "multiple-choice-btn wrong"
+                      : "multiple-choice-btn"
+                    : "multiple-choice-btn"
+                }
               >
                 <span dangerouslySetInnerHTML={{ __html: choice }} />
               </button>
